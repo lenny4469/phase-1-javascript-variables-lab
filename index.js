a0c2ce122f31bd0d2f4b8560f6c2abe28d64785a
Lenny Muriithi Mawira <lenny.mawira@student.moringaschool.com>
2024-03-21 00:55:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+expect (companyName).to.equal('Scuber');
+expect(js).to.match(/const companyName/, "Scruber");
+expect(mostProfitableNeighborhood).to.equal('Chelsea');
+expect(js).to.match(/let mostProfitableNeighborhood/, "Chelsea");
+expect(companyCeo).to.equal('Susan Smith');
+expect(js).to.match(/let companyCeo/, "Susan Smith");
